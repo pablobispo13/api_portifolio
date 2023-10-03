c85c1aa6f0bbb3a4cbdd26ae7ad24b99342c42ce
Pablo Bispo <62626543+pablobispo13@users.noreply.github.com>
2023-10-03 09:35:24 -0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+import express from 'express'
+
+const prisma = new PrismaClient()
+const app = express()
+
+app.use(express.json())
+
+app.get('/users', async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
+
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+})
+
+app.get('/about', (req, res) => {
+  res.send('This is my about route..... ')
+})
+
+app.listen(3000, () =>
+  console.log('REST API server ready at: http://localhost:3000'),
+)
