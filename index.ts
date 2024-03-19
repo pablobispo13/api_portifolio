e7348467bc660704b91bf7d64f3dfe9d88cf2354
Pablo Bispo <62626543+pablobispo13@users.noreply.github.com>
2024-03-19 13:32:12 -0300

    Removendo implementação de autenticação de dois fatores para ser utilizado depois para o projeto de noshop

--- index.ts
+++ index.ts
@@ -1,9 +1,6 @@
 import { PrismaClient } from "./prisma/generated/client";
 import express from "express";
 
-const speakeasy = require("speakeasy");
-const QRCode = require("qrcode");
-
 const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
@@ -29,33 +26,38 @@ app.get(`/useri`, async (req, res) => {
   res.json(result);
 });
 
-// Gera um segredo para o usuário
-app.get("/generateSecret", (req, res) => {
-  const secret = speakeasy.generateSecret();
-  QRCode.toDataURL(secret.otpauth_url, (err: any, dataUrl: any) => {
-    if (err) {
-      res.status(500).send("Erro ao gerar o QR code");
-    } else {
-      res.send({ secret: secret.base32, qrcode: dataUrl });
-    }
-  });
-});
+// Implementação de autenticação de dois fatores utilizando o speakeasy e geração de qrcode
 
-// Verifica o token fornecido pelo usuário
-app.post("/verifyToken", express.json(), (req, res) => {
-  const { secret, token } = req.body;
-  const verified = speakeasy.totp.verify({
-    secret: secret,
-    encoding: "base32",
-    token: token,
-    window: 1, // Permite um atraso de 1 passo para compensar a sincronização do relógio
-  });
-  if (verified) {
-    res.send("Token válido");
-  } else {
-    res.status(401).send("Token inválido");
-  }
-});
+// const speakeasy = require("speakeasy");
+// const QRCode = require("qrcode");
+
+// // Gera um segredo para o usuário
+// app.get("/generateSecret", (req, res) => {
+//   const secret = speakeasy.generateSecret();
+//   QRCode.toDataURL(secret.otpauth_url, (err: any, dataUrl: any) => {
+//     if (err) {
+//       res.status(500).send("Erro ao gerar o QR code");
+//     } else {
+//       res.send({ secret: secret.base32, qrcode: dataUrl });
+//     }
+//   });
+// });
+
+// // Verifica o token fornecido pelo usuário
+// app.post("/verifyToken", express.json(), (req, res) => {
+//   const { secret, token } = req.body;
+//   const verified = speakeasy.totp.verify({
+//     secret: secret,
+//     encoding: "base32",
+//     token: token,
+//     window: 1, // Permite um atraso de 1 passo para compensar a sincronização do relógio
+//   });
+//   if (verified) {
+//     res.send("Token válido");
+//   } else {
+//     res.status(401).send("Token inválido");
+//   }
+// });
 
 app.listen(port, () =>
   console.log(`Servidor rodando em http://localhost:${port}`)
