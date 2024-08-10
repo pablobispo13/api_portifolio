1e7bc39700c57c4e8c0715a3ac0b3b4a86f31229
Pablo Bispo <62626543+pablobispo13@users.noreply.github.com>
2024-08-09 21:49:17 -0300

    Adicionando rota para registrar o token

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "./prisma/generated/client";
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 const app = express();
@@ -25,12 +25,12 @@ app.get(`/validate_token`, async (req: Request, res: Response) => {
 
     return res.json({ valid: false });
   } catch (error) {
-    console.error('Error validating token:', error);
-    return res.status(500).json({ error: 'Internal server error' });
+    console.error("Error validating token:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 });
 
-app.post('/log', async (req: Request, res: Response) => {
+app.post("/log", async (req: Request, res: Response) => {
   const { timestamp, logType, message, userId } = req.body;
 
   try {
@@ -45,8 +45,36 @@ app.post('/log', async (req: Request, res: Response) => {
 
     return res.json({ success: true });
   } catch (error) {
-    console.error('Error logging:', error);
-    return res.status(500).json({ success: false, error: 'Internal server error' });
+    console.error("Error logging:", error);
+    return res
+      .status(500)
+      .json({ success: false, error: "Internal server error" });
+  }
+});
+
+// Rota para registrar o token
+app.post("/register-token", async (req, res) => {
+  const { token, user_id } = req.body;
+
+  if (!token || !user_id) {
+    return res.status(400).json({ error: "Token e user_id são necessários" });
+  }
+
+  try {
+    // Verifique se o token já está registrado
+    const existingEntry = await prisma.user.findUnique({ where: { token } });
+    if (existingEntry) {
+      return res.status(400).json({ error: "Este token já está registrado." });
+    }
+
+    // Adicionar o novo usuário ao banco de dados
+    const newUser = await prisma.user.create({
+      data: { token, userId: user_id },
+    });
+    res.status(201).json({ message: "Bot registrado com sucesso!" });
+  } catch (error) {
+    console.error("Erro ao registrar o token:", error);
+    res.status(500).json({ error: "Erro ao registrar o token." });
   }
 });
 // Implementação de autenticação de dois fatores utilizando o speakeasy e geração de qrcode
